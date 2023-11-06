23cfb0c79a479c7cd7b55a3aa7bc6648dc86ef98
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-11-07 00:07:12 +0800

    compiling widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:dicemo/video_background.dart';
 import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
+import 'package:vibration/vibration.dart';
 import 'dart:math';
+import 'dice.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,43 +31,93 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int rightDice = Random().nextInt(6) + 1;
   int leftDice = Random().nextInt(6) + 1;
+  double turns = 0.0;
+  late ShakeDetector detector;
+  bool rotating = false;
+  bool lucky = false;
+
+  static const double dicePadding = 15.0;
+  static const int turningSpeed = 8;
+
+  @override
+  void initState() {
+    super.initState();
+    detector = ShakeDetector.autoStart(
+      onPhoneShake: rotateDice,
+      minimumShakeCount: 2,
+      shakeSlopTimeMS: 500,          // minimum delay between shakes
+      shakeCountResetTime: 3000,
+      shakeThresholdGravity: 3.2,
+    );
+    print('running init state');
+  }
+
+  @override
+  void dispose() {
+    detector.stopListening();
+    super.dispose();
+  }
+  
   
   void getRandomDiceValues() {
     setState(() {
+      rotating = false;
       rightDice = Random().nextInt(6) + 1;
       leftDice = Random().nextInt(6) + 1;
+      lucky = leftDice == rightDice;
     });
+  }
+
+  void rotateDice() async {
+    if (rotating) return;
+    bool hasVibrator = await Vibration.hasVibrator() as bool;
+    setState(() {
+      rotating = true;
+      turns += turningSpeed;
+    });
+    if (hasVibrator) {
+      Vibration.vibrate();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
-      body: SafeArea(
-        child: Center(
-          child: Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: getRandomDiceValues,
-                    child: Image.asset('images/dice$leftDice.png')
+      body: Stack(
+        children: [
+          VideoBackground(lucky: lucky),
+          SafeArea(
+            child: Center(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(dicePadding),
+                      child: Dice(
+                        turns: turns,
+                        getRandomDiceValues: getRandomDiceValues,
+                        rotateDice: rotateDice, 
+                        diceNum: leftDice,
+                      ),
+                    ),
                   ),
-                ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(dicePadding),
+                      child: Dice(
+                        turns: turns,
+                        getRandomDiceValues: getRandomDiceValues,
+                        rotateDice: rotateDice, 
+                        diceNum: rightDice,
+                      ),
+                    ),
+                  )
+                ],
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextButton(
-                    onPressed: getRandomDiceValues,
-                    child: Image.asset('images/dice$rightDice.png')
-                  ),
-                ),
-              )
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
